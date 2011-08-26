4a5967a60d4be666a9805ca9038d80a18db91277
smathieu <simon.math@gmail.com>
2011-08-26 01:13:04 -0700

    Dumbed down the plugin to address an issue on a project I'm working on. This will likely not work for anything else.

--- lib/guard/sass.rb
+++ lib/guard/sass.rb
@@ -6,112 +6,47 @@ require 'sass'
 
 module Guard
   class Sass < Guard
-  
-    DEFAULTS = {
-      :output => 'css',              # Output directory
-      :notification => true,         # Enable notifications?
-      :shallow  => false,            # Output nested directories?
-      :style => :nested,             # Nested output
-      :debug_info => false,          # File and line number info for FireSass
-      :load_paths => Dir.glob('**/**').find_all {|i| File.directory?(i) }
-    }
-    
-    def initialize(watchers = [], options = {})
-      if options[:input]
-        options[:output] = options[:input] unless options.has_key?(:output)
-        watchers << ::Guard::Watcher.new(%r{^#{options.delete(:input)}/(.+\.s[ac]ss)$})
-      end
-      
-      super(watchers, DEFAULTS.merge(options))
+    def initialize(watchers=[], options={})
+      super
+      # init stuff here, thx!
     end
-        
-            
-    # Builds the sass or scss. Determines engine to use by extension
-    # of path given.
-    #
-    # @param file [String] path to file to build
-    # @return [String] the output css
-    #
-    
-    def build_sass(file)
-      content = File.new(file).read
-      # sass or scss?
-      type = file[-4..-1].to_sym
-      sass_options = {
-        :syntax => type,
-        :load_paths => options[:load_paths],
-        :style => options[:style].to_sym,
-        :debug_info => options[:debug_info],
-      }
-      engine = ::Sass::Engine.new(content, sass_options)
-      engine.render
+
+    # =================
+    # = Guard methods =
+    # =================
+
+    # If one of those methods raise an exception, the Guard::GuardName instance
+    # will be removed from the active guards.
+
+    # Called once when Guard starts
+    # Please override initialize method to init stuff
+    def start
+      Sass::Plugin.update_stylesheets
+      true
     end
-    
-    # Get the file path to output the css based on the file being 
-    # built.
-    #
-    # @param file [String] path to file being built
-    # @return [String] path to file where output should be written
-    #
-    def get_output(file)
-      folder = File.join ::Guard.listener.directory, options[:output]
-        
-      unless options[:shallow]
-        watchers.product([file]).each do |watcher, file|
-          if matches = file.match(watcher.pattern)
-            if matches[1]
-              folder = File.join(::Guard.listener.directory, options[:output], File.dirname(matches[1])).gsub(/\/\.$/, '')
-              break
-            end
-          end
-        end
-      end
-      
-      FileUtils.mkdir_p folder
-      r = File.join folder, File.basename(file).split('.')[0]
-      r << '.css'
+
+    # Called on Ctrl-C signal (when Guard quits)
+    def stop
+      true
     end
-    
-    def ignored?(path)
-      File.basename(path)[0,1] == "_"
+
+    # Called on Ctrl-Z signal
+    # This method should be mainly used for "reload" (really!) actions like reloading passenger/spork/bundler/...
+    def reload
+      true
     end
-    
-    # ================
-    # = Guard method =
-    # ================
-    
-    # Build all files being watched
+
+    # Called on Ctrl-\ signal
+    # This method should be principally used for long action like running all specs/tests/...
     def run_all
-      run_on_change(Watcher.match_files(self, Dir.glob(File.join('**', '[^_]*.*'))))
+      Sass::Plugin.update_stylesheets
+      true
     end
-    
-    # Build the files given
+
+    # Called on file(s) modifications
     def run_on_change(paths)
-      partials = paths.select { |f| ignored?(f) }
-      return run_all unless partials.empty?
-
-      changed_files = paths.reject{ |f| ignored?(f) }.map do |file|
-        css_file = get_output(file)
-        begin
-          File.open(css_file, 'w') {|f| f.write(build_sass(file)) }
-          ::Guard::UI.info "-> rebuilt #{file}", :reset => true
-          ::Guard::Notifier.notify("rebuilt #{file}", :title => "Guard::Sass", :image => :success) if options[:notification]
-          css_file
-        rescue ::Sass::SyntaxError => e
-          ::Guard::UI.error "Sass > #{e.sass_backtrace_str(file)}"
-          ::Guard::Notifier.notify("rebuild failed > #{e.sass_backtrace_str(file)}", :title => "Guard::Sass", :image => :error) if options[:notification]
-          nil
-        end
-      end.compact
-      notify changed_files
+      Sass::Plugin.update_stylesheets
+      true
     end
-    
-    def notify(changed_files)
-      ::Guard.guards.reject{ |guard| guard == self }.each do |guard|
-        paths = Watcher.match_files(guard, changed_files)
-        guard.run_on_change paths unless paths.empty?
-      end
-    end
-
   end
 end
